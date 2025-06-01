7d4e0c4ae25249c1e00aa4cbe81a0bc5ae34d3a8
Mohit Kumar Singh <135017522+mksingh62@users.noreply.github.com>
2025-06-01 21:45:53 +0530

    Update nltk_utils.py

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,29 +1,19 @@
 import numpy as np
 import nltk
-import os
 
-# --- NLTK data setup and download ---
-
-nltk_data_dir = os.path.join(os.getcwd(), "nltk_data")
-os.makedirs(nltk_data_dir, exist_ok=True)
-nltk.data.path.append(nltk_data_dir)
-
-for resource in ['punkt', 'punkt_tab']:
-    try:
-        nltk.data.find(f'tokenizers/{resource}')
-    except LookupError:
-        nltk.download(resource, download_dir=nltk_data_dir)
-
-
+# nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 
 stemmer = PorterStemmer()
 
+
 def tokenize(sentence):
     return nltk.word_tokenize(sentence)
 
+
 def stem(word):
     return stemmer.stem(word.lower())
+
 
 def bag_of_words(tokenized_sentence, words):
     sentence_words = [stem(word) for word in tokenized_sentence]
