a4f9c9ceff04bddb4e2b27912a18eec6da9f3d80
Mohit Kumar Singh <135017522+mksingh62@users.noreply.github.com>
2025-06-01 21:18:50 +0530

    Update nltk_utils.py

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,18 +1,21 @@
-import os
-import nltk
 import numpy as np
-from nltk.stem.porter import PorterStemmer
+import nltk
+import os
 
-# Setup custom nltk_data directory for Render
+# --- NLTK data setup and download ---
+
 nltk_data_dir = os.path.join(os.getcwd(), "nltk_data")
 os.makedirs(nltk_data_dir, exist_ok=True)
 nltk.data.path.append(nltk_data_dir)
 
-# Download 'punkt' tokenizer if missing
-try:
-    nltk.data.find('tokenizers/punkt')
-except LookupError:
-    nltk.download('punkt', download_dir=nltk_data_dir)
+for resource in ['punkt', 'punkt_tab']:
+    try:
+        nltk.data.find(f'tokenizers/{resource}')
+    except LookupError:
+        nltk.download(resource, download_dir=nltk_data_dir)
+
+
+from nltk.stem.porter import PorterStemmer
 
 stemmer = PorterStemmer()
 
