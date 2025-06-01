3026a0a743ca81d5ff14019357b7936c585e1c24
Mohit Kumar Singh <135017522+mksingh62@users.noreply.github.com>
2025-06-01 23:11:22 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,7 @@
+# nltk_setup.py
 import nltk
 import os
 
-nltk_data_dir = os.path.join(os.getcwd(), "nltk_data")
-os.makedirs(nltk_data_dir, exist_ok=True)
-nltk.data.path.append(nltk_data_dir)
-
-# Download required nltk packages only once
-for pkg in ['punkt', 'punkt_tab']:
-    try:
-        nltk.data.find(f'tokenizers/{pkg}')
-    except LookupError:
-        nltk.download(pkg, download_dir=nltk_data_dir)
+nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
+os.makedirs(nltk_data_path, exist_ok=True)
+nltk.download("punkt", download_dir=nltk_data_path)
