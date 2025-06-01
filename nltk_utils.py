c1fe471d517108e39dd79bd684dc6ffdb7db82bf
Mohit Kumar Singh <135017522+mksingh62@users.noreply.github.com>
2025-06-01 22:42:45 +0530

    Update nltk_utils.py

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,19 +1,19 @@
+import os
 import numpy as np
 import nltk
-
-# nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 
-stemmer = PorterStemmer()
+# Ensure NLTK uses the same nltk_data path
+nltk_data_dir = os.path.join(os.getcwd(), "nltk_data")
+nltk.data.path.append(nltk_data_dir)
 
+stemmer = PorterStemmer()
 
 def tokenize(sentence):
     return nltk.word_tokenize(sentence)
 
-
 def stem(word):
     return stemmer.stem(word.lower())
-
 
 def bag_of_words(tokenized_sentence, words):
     sentence_words = [stem(word) for word in tokenized_sentence]
